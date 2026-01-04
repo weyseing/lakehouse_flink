0ca237c003d82d7347ed6d69cff8ea49f7ce75ad
Jeremy Heng <hengweyseing53@gmail.com>
2026-01-04 08:29:04 +0800

    flink dashbaord

--- cli/flink_dashboard.sh
+++ cli/flink_dashboard.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 
 # env
-export $(grep -v '^#' .env | xargs)
+export $(grep -v '^#' ../.env | xargs)
 
-# session name
+# args
 SESSION_NAME=$1
-if [ -z "$SESSION_NAME" ]; then
-    echo "Error: No Flink Session name provided."
+INSTANCE_ID=$2
+LOCAL_PORT=$3
+AWS_PROFILE_ARG=$4
+REGION="ap-southeast-1"
+
+# validate args
+if [ -z "$SESSION_NAME" ] || [ -z "$INSTANCE_ID" ] || [ -z "$LOCAL_PORT" ] || [ -z "$AWS_PROFILE_ARG" ]; then
+    echo "Error: Missing arguments."
+    echo "Usage: ./flink_dashboard.sh <SESSION_NAME> <INSTANCE_ID> <LOCAL_PORT> <AWS_PROFILE>"
+    echo "Example: ./flink_dashboard.sh session_iceberg_1 i-06907bbf06f5e4d93 8081 ssh"
     exit 1
 fi
 
 # tracking url, ip, port
-RAW_URL=$(./cli/ssh_primary_node.sh "yarn application -list" | grep "$SESSION_NAME" | grep -o "http://[^ ]*")
-if [ -z "$RAW_URL" ]; then
-    echo "Error: Could not extract URL for $SESSION_NAME"
+REMOTE_PORT=$(aws ssm send-command \
+    --profile "$AWS_PROFILE_ARG" \
+    --region "$REGION" \
+    --instance-ids "$INSTANCE_ID" \
+    --document-name "AWS-RunShellScript" \
+    --parameters "commands=['yarn application -list | grep $SESSION_NAME | grep -o \":[0-9]\{5\}\" | cut -d: -f2']" \
+    --query "CommandInvocations[0].CommandPlugins[0].Output" \
+    --output text | tr -d '\r' | xargs)
+if [ -z "$REMOTE_PORT" ] || [ "$REMOTE_PORT" == "None" ]; then
+    echo "Error: Could not retrieve YARN URL"
     exit 1
 fi
-REMOTE_IP=$(echo "$RAW_URL" | cut -d'/' -f3 | cut -d':' -f1)
-REMOTE_PORT=$(echo "$RAW_URL" | cut -d':' -f3 | tr -dc '0-9')
-
-# display
-echo "-------------------------------------------"
-echo "Found Session: $SESSION_NAME"
-echo "Internal IP  : $REMOTE_IP"
-echo "Internal Port: $REMOTE_PORT"
-echo "Local Link   : http://localhost:$REMOTE_PORT"
-echo "-------------------------------------------"
+echo "Found Remote Port: $REMOTE_PORT"
+echo "Creating SSM tunnel: http://localhost:$LOCAL_PORT -> $INSTANCE_ID:$REMOTE_PORT"
 
 # ssm port forwarding
+aws ssm start-session \
+    --profile "$AWS_PROFILE_ARG" \
+    --region "$REGION" \
+    --target "$INSTANCE_ID" \
+    --document-name AWS-StartPortForwardingSession \
+    --parameters "{\"portNumber\":[\"$REMOTE_PORT\"],\"localPortNumber\":[\"$LOCAL_PORT\"]}"
