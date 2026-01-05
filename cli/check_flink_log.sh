78e6fa090466278666a5835eadedadcb1a8e4d50
Jeremy Heng <hengweyseing53@gmail.com>
2026-01-06 05:25:09 +0800

    check log

--- cli/check_flink_log.sh
+++ cli/check_flink_log.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# session ID
-SESSION_ID=$1
-if [ -z "$SESSION_ID" ]; then
-    echo "No Session ID provided. Searching for the latest Flink session..."
+# app ID
+APP_ID=$1
+if [ -z "$APP_ID" ]; then
+    echo "No Flink Application ID provided. Please check application ID below"
     yarn application -list
-    exit 0
+    exit 1
 fi
 
 LINES=${2:-100}
-yarn logs -applicationId "$SESSION_ID" | tail -n "$LINES"
+yarn logs -applicationId "$APP_ID" | tail -n "$LINES"
