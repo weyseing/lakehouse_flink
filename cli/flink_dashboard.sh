0e2d0d5de4800f8904ac129f813d65646d1d0057
Jeremy Heng <hengweyseing53@gmail.com>
2026-01-06 11:15:54 +0800

    flink dashboard

--- cli/flink_dashboard.sh
+++ cli/flink_dashboard.sh
@@ -2,28 +2,23 @@
 
 # args
 INSTANCE_ID=$1
-REMOTE_PORT=$2
-LOCAL_PORT=$3
-AWS_PROFILE_ARG=$4
+REMOTE_HOST=$2  
+REMOTE_PORT=$3  
+LOCAL_PORT=$4  
+AWS_PROFILE=$5
 REGION="ap-southeast-1"
 
 # check args
-if [ -z "$INSTANCE_ID" ] || [ -z "$REMOTE_PORT" ] || [ -z "$LOCAL_PORT" ]; then
-    echo "Usage: $0 <INSTANCE_ID> <REMOTE_PORT> <LOCAL_PORT> [AWS_PROFILE]"
-    echo "Example: $0 i-022fa43cdc891111 35265 8081 ssh"
-    exit 1
-fi
-
-# check aws credentials
-if ! aws sts get-caller-identity --profile "$AWS_PROFILE_ARG" --region "$REGION" > /dev/null 2>&1; then
-    echo "Error: Authentication failed. Your session might be expired (check ~/.aws/credentials)."
+if [ -z "$INSTANCE_ID" ] || [ -z "$REMOTE_HOST" ] || [ -z "$REMOTE_PORT" ] || [ -z "$LOCAL_PORT" ] || [ -z "$AWS_PROFILE" ]; then
+    echo "Usage: $0 <INSTANCE_ID> <REMOTE_HOST_IP> <REMOTE_PORT> <LOCAL_PORT> <AWS_PROFILE>"
+    echo "Example: $0 i-022fa43cdc8911111 172.16.15.115 35265 8081 ssh"
     exit 1
 fi
 
 # ssm port forwarding
 aws ssm start-session \
-    --profile "$AWS_PROFILE_ARG" \
+    --profile "$AWS_PROFILE" \
     --region "$REGION" \
     --target "$INSTANCE_ID" \
-    --document-name AWS-StartPortForwardingSession \
-    --parameters "{\"portNumber\":[\"$REMOTE_PORT\"],\"localPortNumber\":[\"$LOCAL_PORT\"]}"
+    --document-name AWS-StartPortForwardingSessionToRemoteHost \
+    --parameters "{\"host\":[\"$REMOTE_HOST\"],\"portNumber\":[\"$REMOTE_PORT\"],\"localPortNumber\":[\"$LOCAL_PORT\"]}"
